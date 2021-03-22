435035d3b3c5cbef81f2e3f00df671be4052a7cb
Daniel Gosselin <dangoscode@gmail.com>
2021-03-22 13:55:37 -0600

    Add Blog, Schedule Model. FK to User, Packages. Update .gitignore. Add .env

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import os
+from datetime import timedelta
 
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_cors import CORS
+from flask_bcrypt import Bcrypt
+
 
 app = Flask(__name__)
 CORS(app)
@@ -12,9 +15,13 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
     os.path.join(basedir, 'app.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = "secret"
+app.secret_key = os.environ.get('SECRET_KEY')
+
+
+CORS(app, supports_credentials=True)
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+flask_bcrypy = Bcrypt(app)
 
 
 class User(db.Model):
@@ -23,18 +30,33 @@ class User(db.Model):
     email = db.Column(db.String(50), nullable=False, unique=True)
     password = db.Column(db.String(100), nullable=False)
     profiles = db.relationship('Profile', backref='user', lazy=True)
+    blogs = db.relationship('Blog', backref='user', lazy=True)
+    schedules = db.relationship('Schedule', backref='user', lazy=True)
 
     def __init__(self, userName, email, password):
         self.userName = userName
         self.email = email
         self.password = password
-       
 
 
 class Profile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     state = db.Column(db.String(2), nullable=True)
     country = db.Column(db.String(4), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(250), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
+class Schedule(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(35), nullable=False)
+    description = db.Column(db.String(35), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 
